d0a5d6763fb98bbd17220e8191eb340baad920b8
Brayan1951 <brayanc1951@gmail.com>
2022-04-14 23:54:58 -0400

    hash routerpo browser router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../components/loggin/LoginScreen';
 import { DashBoardRotes } from './DashBoardRotes';
 
 export const AppRouter = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route exact path='/integrantes/' element={<DashBoardRotes />} />
+                <Route exact path='/integrantes' element={<DashBoardRotes />} />
                 <Route exact path='/*' element={<LoginScreen />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
